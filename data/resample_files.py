37abd4520c0a608ae0337ad5799b21da00b08e50
Jaime Pineda <jaime.e.pineda@gmail.com>
2022-06-07 11:05:09 +0200

    Updated calculations
    -Notebook runs the initial plots and calculations using the matched maps

--- data/resample_files.py
+++ data/resample_files.py
@@ -4,9 +4,11 @@ from reproject import reproject_interp
 
 file_col = 'HGBS_perseus_hires_column_density_map.fits'
 file_temp = 'HGBS_perseus_dust_temperature_map.fits'
-file_DCOp = 'NGC1333_DCOp_H13COp_ratio_coldens.fits'
+
 file_C18O_TdV = 'NGC1333_C18O_3-2_TdV.fits'
 file_C18O = 'NGC1333_C18O_3-2_Ncol.fits'
+
+file_DCOp = 'NGC1333_DCOp_matched_TdV.fits'
 
 file_col_zoom = 'NGC1333_SE_H2.fits'
 file_temp_zoom = 'NGC1333_SE_Tdust.fits'
@@ -27,8 +29,8 @@ N_H2_rep, footprint = reproject_interp(file_col, hd_target)
 fits.writeto(file_col_zoom, N_H2_rep, hd_target, overwrite=True)
 
 hd_target['BUNIT'] = ('K', 'T_dust (SED)')
-hd_target['BMAJ'] = 31.0/3600.
-hd_target['BMIN'] = 31.0/3600.
+hd_target['BMAJ'] = 36.1/3600.
+hd_target['BMIN'] = 36.1/3600.
 Td_rep, footprint = reproject_interp(file_temp, hd_target)
 
 fits.writeto(file_temp_zoom, Td_rep, hd_target, overwrite=True)
