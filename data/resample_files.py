6a2bac6bca1347ba34c3b116b9edc2e8115e54a0
Jaime Pineda <jaime.e.pineda@gmail.com>
2022-06-28 16:57:37 +0200

    Spitzer image -updated script to cutout and resample IRAC images -add FITS files

--- data/resample_files.py
+++ data/resample_files.py
@@ -27,3 +27,30 @@ hd_target['BMIN'] = 36.1/3600.
 Td_rep, footprint = reproject_interp(file_temp, hd_target)
 
 fits.writeto(file_temp_zoom, Td_rep, hd_target, overwrite=True)
+
+
+file_in_IRAC = ['PER_W_COMB_IRAC1_mosaic.fits', 
+        'PER_W_COMB_IRAC2_mosaic.fits',
+        'PER_W_COMB_IRAC3_mosaic.fits',
+        'PER_W_COMB_IRAC4_mosaic.fits']
+file_out_IRAC = ['NGC1333_IRAC1.fits',
+                 'NGC1333_IRAC2.fits',
+                 'NGC1333_IRAC3.fits',
+                 'NGC1333_IRAC4.fits']
+
+
+hd_ngc1333 = fits.getheader(file_DCOp)
+hd_ngc1333['NAXIS1'] = 991
+hd_ngc1333['NAXIS2'] = 1333
+
+hd_ngc1333['CRVAL1'] = 52.2149308
+hd_ngc1333['CRVAL2'] = 31.2818119
+hd_ngc1333['CRPIX1'] = 495
+hd_ngc1333['CRPIX2'] = 666
+hd_ngc1333['CDELT1'] = -0.00033773
+hd_ngc1333['CDELT2'] = 0.00033773
+
+for file_in, file_out in zip(file_in_IRAC, file_out_IRAC):
+    #
+    resampled_image, footprint = reproject_interp(file_in, hd_ngc1333)
+    fits.writeto(file_out, resampled_image, hd_ngc1333, overwrite=True)
